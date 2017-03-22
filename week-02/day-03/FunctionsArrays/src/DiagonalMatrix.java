20bf6aee456b6904f3a3e73249d4a6ff4ea14d8a
Milan Kovacs <kovacsmilan85@gmail.com>
2017-03-22 12:29:53 +0100

    added Java Arrays Exercises

---
+++ week-02/day-03/FunctionsArrays/src/DiagonalMatrix.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+// - Create (dynamically) a two dimensional array
+//   with the following matrix. Use a loop!
+//
+//   1 0 0 0
+//   0 1 0 0
+//   0 0 1 0
+//   0 0 0 1
+//
+// - Print this two dimensional array to the output
+public class DiagonalMatrix {
+  public static void main(String[] args) {
+    int[][] matrix = new int[4][4];
+
+    for (int i = 0; i < matrix.length; i++)
+    {
+      for (int j = 0; j < matrix[i].length; j++)
+      {
+        if (i == j)
+          matrix[i][j] = 1;
+        else
+          matrix[i][j] = 0;
+      }
+    }
+
+    for (int[] line : matrix)
+      System.out.println(Arrays.toString(line));
+  }
+}
---
+++ week-02/day-03/FunctionsArrays/src/PrintAll.java
@@ -0,0 +1,13 @@
+// - Create an array variable named `af`
+//   with the following content: `[4, 5, 6, 7]`
+// - Print all the elements of `af`
+public class PrintAll {
+  public static void main(String[] args) {
+    int af[] = {4, 5, 6, 7};
+
+    for(int elem : af)
+    {
+      System.out.println(elem);
+    }
+  }
+}
