76f35a6ebc950ddecd5ee77f8531d37bb619df83
Milan Kovacs <kovacsmilan85@gmail.com>
2017-03-28 11:44:21 +0200

    Drawing positioned squares

---
+++ week-03/day-02/Graphics/src/PositionSquare.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class PositionSquare {
+
+  public static void mainDraw(Graphics graphics){
+    // create a square drawing function that takes 2 parameters:
+    // the x and y coordinates of the square's top left corner
+    // and draws a 50x50 square from that point.
+    // draw 3 squares with that function.
+    graphics.setColor(Color.red);
+    drawPositionedRectangle(graphics, 75, 75);
+    drawPositionedRectangle(graphics, 225, 75);
+    drawPositionedRectangle(graphics, 75, 225);
+
+  }
+
+  private static void drawPositionedRectangle(Graphics graphics, int x, int y) {
+    graphics.fillRect(x - 25, y - 25, 50, 50);
+  }
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Position Square");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+
+}
