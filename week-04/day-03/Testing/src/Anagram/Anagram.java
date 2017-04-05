c41c5cda151ef1ab046af40d2b5e1c2ae0d9a9d0
Milan Kovacs <kovacsmilan85@gmail.com>
2017-04-05 12:26:39 +0200

    testing Anagrams

---
+++ week-04/day-03/Testing/src/Anagram/Anagram.java
@@ -0,0 +1,18 @@
+package Anagram;
+
+import java.util.Arrays;
+
+public class Anagram {
+  public char[] getCharacters(String line)
+  {
+    line = line.replaceAll("\\s+", "");
+    line = line.toLowerCase();
+    char[] result = line.toCharArray();
+    Arrays.sort(result);
+
+    return result;
+  }
+  public boolean areAnagrams(String s1, String s2) {
+    return Arrays.equals(getCharacters(s1), getCharacters(s2));
+  }
+}
---
+++ week-04/day-03/Testing/src/Anagram/AnagramTest.java
@@ -0,0 +1,51 @@
+package Anagram;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AnagramTest {
+  Anagram anagram = new Anagram();
+
+  @Test
+  public void TestWithMonica()
+  {
+    boolean result = anagram.areAnagrams("Monica Lewinsky", "Nice Silky Woman");
+    assertTrue(result);
+  }
+
+  @Test
+  public void TestWithMachines()
+  {
+    boolean result = anagram.areAnagrams("Slot Machines", "Cash lost in me");
+    assertTrue(result);
+  }
+
+  @Test
+  public void TestWithThirteen()
+  {
+    boolean result = anagram.areAnagrams("Eleven plus Two", "Twelve plus One");
+    assertTrue(result);
+  }
+
+  @Test
+  public void TestWithBanderas()
+  {
+    boolean result = anagram.areAnagrams("Antonio Banderas", "No brains on a date");
+    assertTrue(result);
+  }
+
+  @Test
+  public void TestWithEastwood()
+  {
+    boolean result = anagram.areAnagrams("Clint Eastwood", "Old West action");
+    assertTrue(result);
+  }
+
+  @Test
+  public void TestWithElvis()
+  {
+    boolean result = anagram.areAnagrams("Elvis", "lives");
+    assertTrue(result);
+  }
+}
