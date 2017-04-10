90f7f97f44a221249d3af89486a02c2d34a2a960
Milan Kovacs <kovacsmilan85@gmail.com>
2017-04-10 14:25:49 +0200

    draw prebuilt map

--- week-05/RPG/src/Area.java
+++ week-05/RPG/src/Area.java
@@ -1,15 +1,26 @@
-import com.sun.corba.se.impl.orbutil.graph.Graph;
-
 import java.awt.*;
+
 
 public class Area {
   int[][] tileset;
   Tile tileFloor, tileWall;
 
   final static int TILES = 10;
+  final static int[][] LEVEL = {
+    {0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
+    {0, 0, 0, 1, 0, 1, 0, 1, 1, 0},
+    {0, 1, 1, 1, 0, 1, 0, 1, 1, 0},
+    {0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+    {1, 1, 1, 1, 0, 1, 1, 1, 1, 0},
+    {0, 1, 0, 1, 0, 0, 0, 0, 1, 0},
+    {0, 1, 0, 1, 0, 1, 1, 0, 1, 0},
+    {0, 0, 0, 0, 0, 1, 1, 0, 1, 0},
+    {0, 1, 1, 1, 0, 0, 0, 0, 1, 0},
+    {0, 0, 0, 1, 0, 1, 1, 0, 0, 0}
+  };
 
   public Area() {
-    tileset = new int[TILES][TILES];
+    tileset = LEVEL;
     tileFloor = new TileFloor();
     tileWall = new TileWall();
   }
@@ -17,7 +28,7 @@ public class Area {
   public void draw(Graphics g) {
     for (int i = 0; i < TILES; i++) {
       for (int j = 0; j < TILES; j++) {
-        if (tileset[i][j] == 0) {
+        if (tileset[j][i] == 0) {
           tileFloor.draw(g, i * Tile.SIZE, j * Tile.SIZE);
         } else {
           tileWall.draw(g, i * Tile.SIZE, j * Tile.SIZE);
