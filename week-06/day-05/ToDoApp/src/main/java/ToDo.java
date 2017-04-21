0d97bf8bb19266887c880b7cfe29eb5d1e23b27b
Milan Kovacs <kovacsmilan85@gmail.com>
2017-04-21 11:03:09 +0200

    add dates to ToDo items

--- week-06/day-05/ToDoApp/src/main/java/ToDo.java
+++ week-06/day-05/ToDoApp/src/main/java/ToDo.java
@@ -1,10 +1,16 @@
+import java.time.LocalDateTime;
+
 public class ToDo {
   private String taskDescription;
+  LocalDateTime createdAt;
+  LocalDateTime completedAt;
+
   final static String DONE = "[x] ";
   final static String UNDONE = "[ ] ";
 
   public ToDo(String description) {
-    setTask(description);
+    taskDescription = description;
+    createdAt = LocalDateTime.now();
   }
 
   public String getTask() {
@@ -17,5 +23,19 @@ public class ToDo {
 
   public void check() {
     taskDescription = DONE + taskDescription.substring(UNDONE.length());
+    completedAt = LocalDateTime.now();
   }
+
+  public LocalDateTime complitionTime() {
+    if (completedAt == null) {
+      return null;
+    } else {
+      int year = completedAt.getYear() - createdAt.getYear();
+      int month = completedAt.getMonthValue() - createdAt.getMonthValue();
+      int day = completedAt.getDayOfMonth() - createdAt.getDayOfMonth();
+      int hour = completedAt.getHour() - createdAt.getHour();
+      int minute = completedAt.getMinute() - createdAt.getMinute();
+      return LocalDateTime.of(year, month, day, hour, minute);
+    }
+    }
 }
