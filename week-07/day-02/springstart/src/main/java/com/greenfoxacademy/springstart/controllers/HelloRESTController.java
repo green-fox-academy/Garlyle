260b2468774ccf7e294cf68058422399ac773555
Milan Kovacs <kovacsmilan85@gmail.com>
2017-05-02 15:08:35 +0200

    working REST greet counter and Web App

--- week-07/day-02/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-02/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -4,12 +4,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class HelloRESTController {
+  private final AtomicLong counter = new AtomicLong();
+  private final HashMap<String, AtomicLong> greetingTable = new HashMap<>();
+
   @RequestMapping("/greeting")
   public Greeting greeting(@RequestParam(value="name") String name) {
-    Greeting hi = new Greeting(1, "Hello " + name + "!");
-
+    if (!greetingTable.containsKey(name)) {
+      greetingTable.put(name, new AtomicLong());
+    }
+    long value = greetingTable.get(name).incrementAndGet();
+    //long value = counter.incrementAndGet();
+    Greeting hi = new Greeting(value, "Hello " + name + "!");
     return hi;
   }
 }
