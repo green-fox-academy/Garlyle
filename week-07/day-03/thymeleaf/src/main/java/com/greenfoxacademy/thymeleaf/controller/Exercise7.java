3d083d5adfb8597fa82fa56bc92b032868089ca6
Milan Kovacs <kovacsmilan85@gmail.com>
2017-05-03 12:45:22 +0200

    add Exercise 7

---
+++ week-07/day-03/thymeleaf/src/main/java/com/greenfoxacademy/thymeleaf/controller/Exercise7.java
@@ -0,0 +1,25 @@
+package com.greenfoxacademy.thymeleaf.controller;
+
+import com.greenfoxacademy.thymeleaf.model.BankAccount;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+
+@Controller
+class Exercise7 {
+  final String[] names = {"Simba", "Baloo", "Triton", "Goofy", "Tigger"};
+  final String[] balance = {"2000", "42000", "99999", "1600", "1234"};
+  final String[] type = {"lion", "bear", "King", "dog", "tiger"};
+
+  @RequestMapping("/exercise7")
+  public String listOfAccounts(Model model) {
+    ArrayList<BankAccount> listOfAccounts = new ArrayList<>();
+    for (int i = 0; i < 5; i++) {
+      listOfAccounts.add(new BankAccount(names[i], balance[i], type[i]));
+    }
+    model.addAttribute("accounts", listOfAccounts);
+    return "exercise7";
+  }
+}
