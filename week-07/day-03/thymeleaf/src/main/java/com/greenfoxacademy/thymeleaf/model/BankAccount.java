297cb0de3c032cfc67ff8dda38965af6b7efc1b5
Milan Kovacs <kovacsmilan85@gmail.com>
2017-05-03 11:19:34 +0200

    add exercise 1

---
+++ week-07/day-03/thymeleaf/src/main/java/com/greenfoxacademy/thymeleaf/model/BankAccount.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.thymeleaf.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BankAccount {
+  String name;
+  String balance;
+  String animalType;
+}
