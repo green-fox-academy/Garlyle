6fd1cb5b3cd95a88d6b68795b3b55ca299c31cb5
Milan Kovacs <kovacsmilan85@gmail.com>
2017-05-04 15:36:17 +0200

    create navigation link component

---
+++ week-07/tamagochi/src/main/java/com/greefoxacademy/components/NavLink.java
@@ -0,0 +1,11 @@
+package com.greefoxacademy.components;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class NavLink {
+  String address;
+  String text;
+}
--- week-07/tamagochi/src/main/java/com/greefoxacademy/controller/MainController.java
+++ week-07/tamagochi/src/main/java/com/greefoxacademy/controller/MainController.java
@@ -1,15 +1,27 @@
 package com.greefoxacademy.controller;
 
 import com.greefoxacademy.components.Fox;
+import com.greefoxacademy.components.NavLink;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class MainController {
   @Autowired
   Fox fox;
+
+  @ModelAttribute
+  public void addAttributes(Model model) {
+    ArrayList<NavLink> links = new ArrayList<>();
+    links.add(new NavLink("/", "Information"));
+    links.add(new NavLink("/nutritionStore", "Nutrion Store"));
+    model.addAttribute("nav", links);
+  }
 
   @RequestMapping("/")
   public String index(Model model) {
