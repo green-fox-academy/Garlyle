2d3ed128707f2d42e49cc6a3a187ae6ecb64995d
Milan Kovacs <kovacsmilan85@gmail.com>
2017-05-05 14:38:07 +0200

    new pet

--- week-07/tamagochi/src/main/java/com/greefoxacademy/config/MainConfig.java
+++ week-07/tamagochi/src/main/java/com/greefoxacademy/config/MainConfig.java
@@ -3,12 +3,14 @@ package com.greefoxacademy.config;
 import com.greefoxacademy.components.Fox;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import java.util.ArrayList;
 
 @Configuration
 class MainConfig {
   @Bean
+  @Primary
   public Fox DefaultFox() {
     Fox myFox = new Fox("Mr. Fox", "images/greenfox.png","pizza", "lemonade");
     myFox.learnTrick("write HTML");
@@ -25,10 +27,20 @@ class MainConfig {
   }
 
   @Bean
+  public Fox Chocobo() {
+    Fox myChocobo = new Fox("Chocobo", "images/chocobo.png", "human", "water");
+    myChocobo.learnTrick("moonwalk");
+    myChocobo.learnTrick("do a barrel roll");
+
+    return myChocobo;
+  }
+
+  @Bean
   public ArrayList<Fox> makeList() {
     ArrayList<Fox> foxes = new ArrayList<>();
     foxes.add(DefaultFox());
     foxes.add(CyberDragon());
+    foxes.add(Chocobo());
 
     return foxes;
   }
