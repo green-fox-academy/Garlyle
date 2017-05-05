050a922211447b8c912abe4e8cb3a5c4b5f773f7
Milan Kovacs <kovacsmilan85@gmail.com>
2017-05-05 12:01:26 +0200

    Fox configs with different images

--- week-07/tamagochi/src/main/java/com/greefoxacademy/components/Fox.java
+++ week-07/tamagochi/src/main/java/com/greefoxacademy/components/Fox.java
@@ -7,12 +7,14 @@ import java.util.ArrayList;
 @Getter
 public class Fox {
   String name;
+  String image;
   ArrayList<String> tricks;
   String food;
   String drink;
 
-  public Fox(String name, String food, String drink) {
+  public Fox(String name, String image, String food, String drink) {
     this.name = name;
+    this.image = image;
     this.food = food;
     this.drink = drink;
     tricks = new ArrayList<>();
--- week-07/tamagochi/src/main/java/com/greefoxacademy/config/MainConfig.java
+++ week-07/tamagochi/src/main/java/com/greefoxacademy/config/MainConfig.java
@@ -3,19 +3,28 @@ package com.greefoxacademy.config;
 import com.greefoxacademy.components.Fox;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 class MainConfig {
   @Bean
   public Fox DefaultFox() {
-    Fox myFox = new Fox("Mr. Fox", "pizza", "lemonade");
+    Fox myFox = new Fox("Mr. Fox", "images/greenfox.png","pizza", "lemonade");
     myFox.learnTrick("write HTML");
     myFox.learnTrick("code in Java");
 
     return myFox;
   }
 
+  @Bean
+  @Primary
+  public Fox CyberDragon() {
+    Fox myDragon = new Fox("Cyber Dragon", "images/cyberdragon.png", "sushi", "gasoline");
+
+    return myDragon;
+  }
+
   public Fox newFox() {
-    return new Fox("Mr. Green", "salad", "water");
+    return new Fox("Mr. Green", "images/greenfox.png","salad", "water");
   }
 }
