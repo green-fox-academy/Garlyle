3e6ac1eb41c24d46429395556928a18e506938a7
Milan Kovacs <kovacsmilan85@gmail.com>
2017-05-09 14:04:34 +0200

    MySQL Todo initial commit

---
+++ week-08/day-02/todo2/src/main/java/com/greenfoxacademy/Todo2Application.java
@@ -0,0 +1,25 @@
+package com.greenfoxacademy;
+
+import com.greenfoxacademy.model.Todo;
+import com.greenfoxacademy.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Todo2Application implements CommandLineRunner {
+	@Autowired
+	TodoRepository repository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Todo2Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		repository.save(new Todo("Finish Green Fox Academy"));
+		repository.save(new Todo("Develop a Video Game"));
+		repository.save(new Todo("Earn money"));
+	}
+}
