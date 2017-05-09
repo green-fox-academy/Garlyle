890de7f3e980d2d94128afd43c585673c021e600
Milan Kovacs <kovacsmilan85@gmail.com>
2017-05-09 11:56:10 +0200

    add controller

---
+++ week-08/day-02/todo/src/main/java/com/greenfoxacademy/controller/TodoController.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/todo")
+public class TodoController {
+  @RequestMapping(value={"/", "/list"})
+  @ResponseBody
+  public String list(Model model) {
+    return "This is my first todo";
+itcd   }
+}
