9cae1ef52c1ca24ae38bf057451d6d6728acc15a
Milan Kovacs <kovacsmilan85@gmail.com>
2017-05-12 11:23:17 +0200

    delete and update mapping

--- week-08/day-05/reddit/src/main/java/com/greenfoxacademy/milan/controller/PostsController.java
+++ week-08/day-05/reddit/src/main/java/com/greenfoxacademy/milan/controller/PostsController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("*")
 public class PostsController {
   @Autowired
   PostRepository repository;
@@ -37,6 +38,24 @@ public class PostsController {
   public Post downvote(@PathVariable Long id) {
     Post post = repository.findOne(id);
     post.downVote();
+    repository.save(post);
+
+    return post;
+  }
+
+  @DeleteMapping("/posts/{id}")
+  public Post deletePost(@PathVariable Long id) {
+    Post post = repository.findOne(id);
+    repository.delete(id);
+
+    return post;
+  }
+
+  @PutMapping("/posts/{id}")
+  public Post updatePost(@PathVariable Long id, @RequestBody PostForm updateForm) {
+    Post post = repository.findOne(id);
+    post.setTitle(updateForm.getTitle());
+    post.setHref(updateForm.getHref());
     repository.save(post);
 
     return post;
