2176c23de5b3151353503dc363803ad71a6878e7
Garlyle <kovacsmilan85@gmail.com>
2017-07-03 14:15:10 +0200

    random ship placement

--- CSharp/BattleShipApplication/BattleShipApplication/GameTable.cs
+++ CSharp/BattleShipApplication/BattleShipApplication/GameTable.cs
@@ -31,6 +31,66 @@ namespace BattleShipApplication
             }
         }
 
+        public void placeShip(int length)
+        {
+            Random rand = new Random();
+            bool orientation;
+            int posX, posY;
+            do
+            {
+                orientation = (rand.Next() % 2 == 0);
+                posX = rand.Next(0, 20);
+                posY = rand.Next(0, 20);
+                if (orientation == true && (posX + length >= 20))
+                {
+                    posX -= length;
+                } 
+                else if (posY + length >= 20)
+                {
+                    posY -= length;
+                }
+            } while (posValid(posX, posY, orientation, length);
+            setupShip(posX, posY, orientation, length);
+        }
+
+        private bool posValid(int posX, int posY, bool orientation, int length)
+        {
+            for (int i = -1; i <= length; i++)
+                for (int j = -1; j <= 1; j++)
+                {
+                    if (orientation)
+                    {
+                        if (shipMap[posX + i, posY + j] == true)
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        if (shipMap[posX + j, posY + i] == true)
+                        {
+                            return false;
+                        }
+                    }
+                }
+            return true;
+        }
+
+        private void setupShip(int posX, int posY, bool orientation, int length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                if (orientation)
+                {
+                    shipMap[posX + i, posY] = true;
+                }
+                else
+                {
+                    shipMap[posX, posY + i] = true;
+                }
+            }
+        }
+
         public bool hit(int x, int y)
         {
             visibilityMap[x, y] = true;
