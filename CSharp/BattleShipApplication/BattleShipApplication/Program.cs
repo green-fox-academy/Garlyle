5cda0f9867bfa160d3c8be5c5fb312f90e15d453
Garlyle <kovacsmilan85@gmail.com>
2017-07-03 14:38:38 +0200

    basic targeting

--- CSharp/BattleShipApplication/BattleShipApplication/Program.cs
+++ CSharp/BattleShipApplication/BattleShipApplication/Program.cs
@@ -5,11 +5,32 @@ namespace BattleShipApplication
 {
     internal class Program
     {
+        private static int hits = 0;
+        private static int misses = 0;
+        private static int total = 0;
+
         public static void Main(string[] args)
         {
             GameTable table = new GameTable();
             table.init();
-            table.printMap();
+
+            while (true)
+            {
+                table.printMap();
+                Console.Write("Target: ");
+                string input = Console.ReadLine();
+                int x = input[0] - 'A';
+                int y = input[1] - 'A';
+                if (table.hit(x, y))
+                {
+                    hits++;
+                }
+                else
+                {
+                    misses++;
+                }
+                total++;
+            }
         }
     }
 }
