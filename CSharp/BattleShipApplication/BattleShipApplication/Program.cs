8a55f1ab0f53b5379cbc9ba8820d35784ae5f946
Garlyle <kovacsmilan85@gmail.com>
2017-07-03 11:41:29 +0200

    initial commit

---
+++ CSharp/BattleShipApplication/BattleShipApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BattleShipApplication
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
