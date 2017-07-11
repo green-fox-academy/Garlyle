0dc8a2629d8f7515f7b592ce5d587e595dbea21f
Garlyle <kovacsmilan85@gmail.com>
2017-07-11 13:08:03 +0200

    fibonacci with test

---
+++ CSharp/EntityFramework/EntityTest/FibonacciTest.cs
@@ -0,0 +1,28 @@
+﻿using EntityFramework;
+using NUnit.Framework;
+
+namespace EntityTest
+{
+    [TestFixture]
+    public class FibonacciTest
+    {
+        [Test]
+        public void TestCorrectNumber()
+        {
+            Assert.AreEqual(8, Program.fibonacci(6));
+        }
+        
+        [Test]
+        public void TestForZero()
+        {
+            Assert.AreEqual(0, Program.fibonacci(0));
+        }
+
+        [Test]
+        public void TestForNegative()
+        {
+            Assert.AreEqual(0, Program.fibonacci(-20));
+        }
+
+    }
+}
