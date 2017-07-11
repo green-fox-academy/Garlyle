59f8687f01daf1e0dd7dfd9ae0895519fb960423
Garlyle <kovacsmilan85@gmail.com>
2017-07-11 11:57:05 +0200

    anagram method with tests

---
+++ CSharp/EntityFramework/EntityFramework/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace EntityFramework
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+
+        public bool areAnagrams(string first, string second)
+        {
+            return Enumerable.SequenceEqual(getCharacters(first), getCharacters(second));
+        }
+
+        private char[] getCharacters(string line)
+        {
+            line = line.ToLower();
+            line = Regex.Replace(line, "\\s+", "");
+            char[] result = line.ToCharArray();
+            Array.Sort(result);
+
+            return result;
+        }
+    }
+}
---
+++ CSharp/EntityFramework/EntityTest/AnagramTests.cs
@@ -0,0 +1,53 @@
+﻿using EntityFramework;
+using NUnit.Framework;
+
+namespace EntityTest
+{
+    [TestFixture]
+    public class AnagramTests
+    {
+        private Program testProgram;
+
+        [SetUp]
+        public void Init()
+        {
+            testProgram = new Program();
+        }
+        
+        [Test]
+        public void TestWithMonica()
+        {
+            Assert.True(testProgram.areAnagrams("Monica Lewinsky", "Nice Silky Woman"));
+        }
+        
+        [Test]
+        public void TestWithMachines()
+        {
+            Assert.True(testProgram.areAnagrams("Slot Machines", "Cash lost in me"));
+        }
+        
+        [Test]
+        public void TestWithThirteen()
+        {
+            Assert.True(testProgram.areAnagrams("Eleven plus Two", "Twelve plus One"));
+        }
+
+        [Test]
+        public void TestWithBanderas()
+        {
+            Assert.True(testProgram.areAnagrams("Antonio Banderas", "No brains on a date"));
+        }
+
+        [Test]
+        public void TestWithEastwood()
+        {
+            Assert.True(testProgram.areAnagrams("Clint Eastwood", "Old West action"));
+        }
+
+        [Test]
+        public void TestWithElvis()
+        {
+            Assert.True(testProgram.areAnagrams("Elvis", "lives"));
+        }
+    }
+}
